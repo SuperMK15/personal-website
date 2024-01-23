21d6a8ae384c3dae3d46fe19d91c09ce38ba5b31
SuperMK15 <smartiskush@gmail.com>
2024-01-22 22:17:49 -0500

    updated aboutme

--- pages/terminal.jsx
+++ pages/terminal.jsx
@@ -1,9 +1,62 @@
 import React, { useEffect, useState } from "react";
+import { TerminalContextProvider, ReactTerminal } from "react-terminal";
 import MatrixRainingCode from '../components/MatrixRainingEffect';
 import styles from '../styles/TerminalPage.module.css';
 
 const TerminalPage = () => {
     const [booted, setBooted] = useState(false);
+    const [username, setUsername] = useState("");
+
+    const commands = {
+        about: (
+            <>
+                Hello, my name is Manasva! I am currently a first-year student at the Univerity of Waterloo, pursuing a Computer Science degree with a Digital Hardware specialization. I'm also a recipient of the Schulich Leader Scholarship, the most prestigious STEM scholarship for Canadian undergraduate students!
+                <br />
+            </>
+        ),
+
+        social: (type) => {
+            if (type === "email") {
+                return "manasva.katyal@gmail.com";
+            } else if (type === "linkedin") {
+                return "https://www.linkedin.com/in/manasva-katyal/";
+            } else if (type === "github") {
+                return "https://github.com/SuperMK15/";
+            } else {
+                return "Please specify a valid social after the command (email, linkedin, or github).";
+            }
+        },
+
+        user: (name) => {
+            setUsername(name);
+            if (!name)
+                return "Username reset.";
+            else
+                return `Username set to ${name}. To reset, run the command 'user' without any arguments.`;
+        },
+
+        exit: () => {
+            window.location.href = "/";
+            return "Exiting terminal...";
+        },
+
+        help: () => (
+            <>
+                <p>----------------------------------------------------------------------------------------</p>
+                <p>Available Commands</p>
+                <p>----------------------------------------------------------------------------------------</p>
+                <ul>
+                    -- about: Displays a short bio about me. <br />
+                    -- social [type]: Displays my email, LinkedIn, and GitHub based on the [type] specified. <br />
+                    -- user [username]: allows you to specify a [username] to be displayed in the terminal prompt. <br />
+                    -- clear: Clears the terminal. <br />
+                    -- exit: Exits the terminal and returns to the home page. <br />
+                    -- help: Displays this help message. <br />
+                </ul>
+                <p>----------------------------------------------------------------------------------------</p>
+            </>
+        ),
+    };
 
     useEffect(() => {
         const bootUp = () => {
@@ -12,33 +65,36 @@ const TerminalPage = () => {
             }, 1800);
         };
 
-        bootUp();
+        if (!booted) bootUp();
     }, []);
 
     return (
-        <div className={booted ? styles.terminalOverlay : styles.bootUpOverlay}>
-            <MatrixRainingCode className={styles.matrixContainer} />
-            {booted ? (
-                <div className={styles.centerText}>
-                    <div className={styles.textContainer}>
-                        <h1 className={styles.textMessage}>
-                            Terminal Coming Soon!
-                        </h1>
-                        <a href="./" className={styles.linkBack}>
-                            Go back to Home
-                        </a>
+        <TerminalContextProvider>
+            <div className={booted ? styles.terminalOverlay : styles.bootUpOverlay}>
+                <MatrixRainingCode className={styles.matrixContainer} />
+                {booted ? (
+                    <div className={styles.cmd}>
+                        <div className={styles.cmdContainer}>
+                            <ReactTerminal
+                                commands={commands}
+                                theme="matrix"
+                                prompt={username + "$"}
+                                welcomeMessage={<>Welcome to the terminal! Type 'help' see a list of commands.<br /></>}
+                                showControlButtons={false}
+                            />
+                        </div>
                     </div>
-                </div>
-            ) : (
-                <div className={styles.centerText}>
-                    <div className={styles.textContainer}>
-                        <h1 className={styles.textMessage}>
-                            Booting Up...
-                        </h1>
+                ) : (
+                    <div className={styles.centerText}>
+                        <div className={styles.textContainer}>
+                            <h1 className={styles.textMessage}>
+                                Booting Up...
+                            </h1>
+                        </div>
                     </div>
-                </div>
-            )}
-        </div>
+                )}
+            </div>
+        </TerminalContextProvider>
     );
 };
 
