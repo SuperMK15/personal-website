2858c953ad36048aa520051433166f1a7d700fc4
SuperMK15 <smartiskush@gmail.com>
2024-02-01 12:38:42 -0500

    added vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,14 @@ import Layout from "../components/Layout";
 import Head from "../components/Head";
 import "../styles/globals.css";
 
+import { Analytics } from '@vercel/analytics/react';
+
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Head title={`Manasva Katyal | ${pageProps.title}`} />
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
   );
 }
