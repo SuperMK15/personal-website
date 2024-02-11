5afeee066be26ae7905cd0f9ac57d2c7581db94c
SuperMK15 <smartiskush@gmail.com>
2024-02-10 21:27:34 -0500

    big terminal update!

--- pages/terminal.jsx
+++ pages/terminal.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { TerminalContextProvider, ReactTerminal } from "react-terminal";
 import MatrixRainingCode from '../components/MatrixRainingEffect';
 import styles from '../styles/TerminalPage.module.css';
+import { BarLoader } from 'react-spinners';
 
 import projects from './data/projects.json';
 import experiences from './data/experiences.json';
@@ -10,33 +11,52 @@ import certifications from './data/certifications.json';
 const TerminalPage = () => {
     const [booted, setBooted] = useState(false);
     const [username, setUsername] = useState("");
+    const [currentDirectory, setCurrentDirectory] = useState("");
+
+    let directories = {
+        "/home": ["about.txt", "projects/", "contact/", "experiences/", "certifications/"], // root directory
+        "/projects": [...projects.map(project => project.terminal_name + "/")],
+        "/contact": ["email.txt", "linkedin.txt", "github.txt"],
+        "/experiences": [...experiences.map(experience => experience.terminal_name + "/")],
+        "/certifications": [...certifications.map(certification => certification.terminal_name + "/")],
+    };
+
+    projects.forEach(project => {
+        directories["/projects/" + project.terminal_name] =
+            [
+                "name.txt",
+                "description.txt",
+                "techstack.txt",
+                (project.source_code ? "project_github.exe" : null),
+                (project.demo ? "demo.exe" : null),
+                (project.pdf ? "research_paper.exe" : null)
+            ].filter(Boolean);
+    });
+
+    experiences.forEach(experience => {
+        directories["/experiences/" + experience.terminal_name] =
+            [
+                "role.txt",
+                "duration.txt",
+                "role_description.txt"
+            ];
+    });
+
+    certifications.forEach(certification => {
+        directories["/certifications/" + certification.terminal_name] =
+            [
+                "title.txt",
+                "credential.exe"
+            ];
+    });
 
     const commands = {
-        about: (
-            <>
-                Hello, my name is Manasva! I am currently a first-year student at the Univerity of Waterloo, pursuing a Computer Science degree with a Digital Hardware specialization. I'm also a recipient of the Schulich Leader Scholarship, the most prestigious STEM scholarship for Canadian undergraduate students!
-                <br />
-            </>
-        ),
-
-        social: (type) => {
-            if (type === "email") {
-                return "manasva.katyal@gmail.com";
-            } else if (type === "linkedin") {
-                return "https://www.linkedin.com/in/manasva-katyal/";
-            } else if (type === "github") {
-                return "https://github.com/SuperMK15/";
-            } else {
-                return "Please specify a valid social after the command (email, linkedin, or github).";
-            }
-        },
-
         user: (name) => {
             setUsername(name);
             if (!name)
-                return "Username reset.";
+                return <p>Username reset.</p>;
             else
-                return `Username set to ${name}. To reset, run the command 'user' without any arguments.`;
+                return <p>{`Username set to ${name}. To reset, run the command 'user' without any arguments.`}</p>;
         },
 
         exit: () => {
@@ -55,8 +75,11 @@ const TerminalPage = () => {
                 <p>Available Commands</p>
                 <p>----------------------------------------------------------------------------------------</p>
                 <ul>
-                    -- about: Displays a short bio about me. <br />
-                    -- social [type]: Displays my email, LinkedIn, and GitHub based on the [type] specified. <br />
+                    -- ls: Lists all files and directories in the current directory. <br />
+                    -- cd [directory]: Changes the current directory to the specified [directory]. <br />
+                    -- pwd: Displays the current directory. <br />
+                    -- cat [file]: Displays the contents of the specified [file]. <br />
+                    -- exec [file]: Executes the specified [file]. Must be a .exe file to do so. <br />
                     -- user [username]: allows you to specify a [username] to be displayed in the terminal prompt. <br />
                     -- clear: Clears the terminal. <br />
                     -- exit: Exits the terminal and returns to the home page. <br />
@@ -65,6 +88,174 @@ const TerminalPage = () => {
                 <p>----------------------------------------------------------------------------------------</p>
             </>
         ),
+
+        "download-resume": () => {
+            const link = document.createElement("a");
+            link.href = "https://drive.google.com/uc?export=download&id=1h6b5aXJrZPdmlCJf2oF9NuVBUucAVotR";
+            link.setAttribute("download", "Manasva_Katyal_Resume.pdf");
+            link.style.display = "none"; // Hide the link
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link); // Remove the link from the document after download starts
+            return "Downloading resume...";
+        },
+
+        cd: (arg) => {
+            if (!arg || arg === "~") {
+                setCurrentDirectory("");
+                return <p></p>;
+            }
+
+            let nav_tracker;
+
+            if (arg[0] === '/') {
+                nav_tracker = arg;
+            } else {
+                if (arg[0] === "." && arg[1] == "/") arg = arg.slice(2);
+
+                const commands = arg.split("/").filter(Boolean);
+                nav_tracker = currentDirectory;
+
+                commands.forEach(command => {
+                    if (command === "..") {
+                        if (currentDirectory === "/") {
+                            return <p>{`Directory ${arg} not found.`}</p>;
+                        } else {
+                            nav_tracker = nav_tracker.slice(0, nav_tracker.lastIndexOf("/"));
+                        }
+                    } else {
+                        nav_tracker += "/" + command;
+                    }
+                });
+            }
+
+            if (nav_tracker === "") {
+                setCurrentDirectory(nav_tracker);
+                return <p></p>;
+            } else if (nav_tracker in directories) {
+                setCurrentDirectory(nav_tracker);
+                return <p></p>;
+            } else {
+                return <p>{`Directory ${arg} not found.`}</p>;
+            }
+        },
+
+        ls: () => {
+            if (currentDirectory === "") {
+                let ret = "";
+                directories["/home"].forEach(element => {
+                    ret += element + '\u00A0\u00A0';
+                });
+                return <p>{ret}</p>;
+            } else {
+                let ret = "";
+                directories[currentDirectory].forEach(element => {
+                    ret += element + '\u00A0\u00A0';
+                });
+                return <p>{ret}</p>;
+            }
+        },
+
+        pwd: () => {
+            if (currentDirectory === "") {
+                return <p>~/</p>;
+            } else {
+                return <p>{"~" + currentDirectory}</p>;
+            }
+        },
+
+        cat: (file) => {
+            if (file === "about.txt") {
+                if (currentDirectory !== "") return <p>{"File " + file + " not found."}</p>;
+                else return (
+                    <>
+                        Hello, my name is Manasva! I am currently a first-year student at the Univerity of Waterloo, pursuing a Computer Science degree with a Digital Hardware specialization. I'm also a recipient of the Schulich Leader Scholarship, the most prestigious STEM scholarship for Canadian undergraduate students!
+                        <br />
+                    </>
+                );
+            } else if (file === "email.txt") {
+                return <p>manasva.katyal@gmail.com</p>
+            } else if (file === "linkedin.txt") {
+                return <p>https://www.linkedin.com/in/manasva-katyal/</p>
+            } else if (file === "github.txt") {
+                return <p>https://github.com/SuperMK15/</p>
+            } else if (file === "name.txt") {
+                const ret = projects.find(project => "/projects/" + project.terminal_name === currentDirectory);
+                if (ret?.name) return <p>{ret.name}</p>;
+            } else if (file === "description.txt") {
+                const ret = projects.find(project => "/projects/" + project.terminal_name === currentDirectory);
+                if (ret?.description) return <p>{ret.description}</p>;
+            } else if (file === "techstack.txt") {
+                const ret = projects.find(project => "/projects/" + project.terminal_name === currentDirectory);
+                if (ret?.tags) {
+                    let techstack = "";
+                    ret.tags.forEach(tag => {
+                        techstack += tag + ',\u00A0';
+                    });
+                    techstack = techstack.slice(0, -2);
+                    return <p>{techstack}</p>;
+                }
+            } else if (file === "project_github.exe") {
+                const ret = projects.find(project => "/projects/" + project.terminal_name === currentDirectory);
+                if (ret?.source_code) return <p>goto: {ret.source_code}</p>;
+            } else if (file === "demo.exe") {
+                const ret = projects.find(project => "/projects/" + project.terminal_name === currentDirectory);
+                if (ret?.demo) return <p>goto: {ret.demo}</p>;
+            } else if (file === "research_paper.exe") {
+                const ret = projects.find(project => "/projects/" + project.terminal_name === currentDirectory);
+                if (ret?.pdf) return <p>goto: {ret.pdf}</p>;
+            } else if (file === "role.txt") {
+                const ret = experiences.find(experience => "/experiences/" + experience.terminal_name === currentDirectory);
+                if (ret?.title) return <p>{ret.title}</p>;
+            } else if (file === "duration.txt") {
+                const ret = experiences.find(experience => "/experiences/" + experience.terminal_name === currentDirectory);
+                if (ret?.date) return <p>{ret.date}</p>;
+            } else if (file === "role_description.txt") {
+                const ret = experiences.find(experience => "/experiences/" + experience.terminal_name === currentDirectory);
+                if (ret?.description) return <p>{ret.description}</p>;
+            } else if (file === "title.txt") {
+                const ret = certifications.find(certification => "/certifications/" + certification.terminal_name === currentDirectory);
+                console.log(ret);
+                if (ret?.name) return <p>{ret.name}</p>;
+            } else if (file === "credential.exe") {
+                const ret = certifications.find(certification => "/certifications/" + certification.terminal_name === currentDirectory);
+                if (ret?.credential_link) return <p>goto: {ret.credential_link}</p>;
+            }
+
+            return <p>{`File ${file} not found.`}</p>;
+        },
+
+        exec: (file) => {
+            const ret_val = <p>Running executable...</p>
+
+            if (file === "project_github.exe") {
+                const ret = projects.find(project => "/projects/" + project.terminal_name === currentDirectory);
+                if (ret?.source_code) {
+                    window.open(ret.source_code, "_blank");
+                    return ret_val;
+                }
+            } else if (file === "demo.exe") {
+                const ret = projects.find(project => "/projects/" + project.terminal_name === currentDirectory);
+                if (ret?.demo) {
+                    window.open(ret.demo, "_blank");
+                    return ret_val;
+                }
+            } else if (file === "research_paper.exe") {
+                const ret = projects.find(project => "/projects/" + project.terminal_name === currentDirectory);
+                if (ret?.pdf) {
+                    window.open(ret.pdf, "_blank");
+                    return ret_val;
+                }
+            } else if (file === "credential.exe") {
+                const ret = certifications.find(certification => "/certifications/" + certification.terminal_name === currentDirectory);
+                if (ret?.credential_link) {
+                    window.open(ret.credential_link, "_blank");
+                    return ret_val;
+                }
+            }
+            
+            return <p>{`File ${file} not found or is not a .exec file.`}</p>;
+        }
     };
 
     useEffect(() => {
@@ -87,20 +278,19 @@ const TerminalPage = () => {
                             <ReactTerminal
                                 commands={commands}
                                 theme="matrix"
-                                prompt={username + "$"}
-                                welcomeMessage={<>Welcome to the terminal! Type 'help' see a list of commands.<br /></>}
+                                prompt={username + (username ? ":" : "") + "~" + currentDirectory + "$"}
+                                welcomeMessage={<p>Welcome to the terminal! Type 'help' see a list of commands.</p>}
                                 showControlButtons={false}
                                 showControlBar={false}
                                 style={{ overflow: "hidden" }}
+                                errorMessage={<p>Command not found. Type 'help' to see a list of commands.</p>}
                             />
                         </div>
                     </div>
                 ) : (
                     <div className={styles.centerText}>
                         <div className={styles.textContainer}>
-                            <h1 className={styles.textMessage}>
-                                Booting Up...
-                            </h1>
+                                <BarLoader color={'#00FF00'} loading={true} size={100} />
                         </div>
                     </div>
                 )}
